3061a29605833034a654a691511af887da0d5355
srinivas <srinivasgoud6547@gmail.com>
2024-01-19 12:43:43 +0530

    Text Basede Adventure Game

---
+++ textbaseAdvgame.py
@@ -0,0 +1,35 @@
+import random
+
+name = input("What is your name: ")
+print("Good Luck ! ", name)
+words = ['book','zero','shirt','bike','rainbow', 'computer', 'science', 'programming',
+		'python', 'mathematics', 'player', 'condition',
+		'reverse', 'water', 'board','king','queen','hero']
+word = random.choice(words)
+print(word)
+print("Guess the characters")
+guesses = ''
+turns = 12
+while turns > 0:
+	failed = 0
+	for char in word:
+		if char in guesses:
+			print(char, end=" ")
+
+		else:
+			print("_")
+			failed += 1
+	if failed == 0:
+		print("You Win")
+		print("The word is: ", word)
+		break
+	print()
+	guess = input("guess a character:")
+	guesses += guess
+	if guess not in word:
+		turns -= 1
+		print("Wrong")
+		print("You have", + turns, 'more guesses')
+
+		if turns == 0:
+			print("You Loose")
